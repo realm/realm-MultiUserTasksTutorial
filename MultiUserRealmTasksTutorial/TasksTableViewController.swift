e68477a087a3d64dc11171f3ee28d2be244102c6
Ian Ward <ward.ianp@gmail.com>
2017-12-20 16:50:56 -0800

    realm edits

--- MultiUserRealmTasksTutorial/TasksTableViewController.swift
+++ MultiUserRealmTasksTutorial/TasksTableViewController.swift
@@ -2,37 +2,26 @@
 //  TasksTableViewController.swift
 //  MultiUserRealmTasksTutorial
 //
-//  Created by David Spector on 6/6/17.
-//  Copyright © 2017 Realm. All rights reserved.
+//  Created by Ian Ward on 12/20/17.
+//  Copyright © 2017 Ian Ward. All rights reserved.
 //
 
 import UIKit
 import RealmSwift
 
-class TasksTableViewController: UITableViewController,  UIGestureRecognizerDelegate  {
-    var realm: Realm!
-    var notificationToken: NotificationToken!
+class TasksTableViewController: UITableViewController {
     var items = List<Task>()
+    var notificationToken: NotificationToken!
+    var realm: Realm!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupUI()
         setupRealm()
         
         items.append(Task(value: ["text": "My First Task"]))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-    
-    
-    // MARK: - Realm Specific Code
-    
     func setupUI() {
         title = "My Tasks"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
@@ -46,17 +35,18 @@ class TasksTableViewController: UITableViewController,  UIGestureRecognizerDeleg
         navItem.rightBarButtonItems =  [addButton, logoutButton]
         navItem.leftBarButtonItem = editButtonItem
         navBar.setItems([navItem], animated: false)
-
+        
         self.view.addSubview(navBar)
     }
-
+    
+    
     
     func setupRealm() {
+        
         DispatchQueue.main.async {
             // Open Realm
             self.realm = try! Realm(configuration: tasksRealmConfig(user: SyncUser.current!))
-            print(self.realm)
-            
+        
             // Show initial tasks
             func updateList() {
                 if self.items.realm == nil, let list = self.realm.objects(TaskList.self).first {
@@ -67,76 +57,55 @@ class TasksTableViewController: UITableViewController,  UIGestureRecognizerDeleg
             updateList()
             
             // Notify us when Realm changes
-            self.notificationToken = self.realm.observe{realm in
+            self.notificationToken = self.realm.observe { _,_ in
                 updateList()
-                print("here in notifications")
             }
         } // of Dispatch...main
     }// of setupRealm
 
-
-    func add() {
-        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
-        var alertTextField: UITextField!
-        alertController.addTextField { textField in
-            alertTextField = textField
-            textField.placeholder = "Task Name"
-        }
-        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
-            guard let text = alertTextField.text , !text.isEmpty else { return }
-            
-            let items = self.items
-            try! items.realm?.write {
-                items.insert(Task(value: ["text": text]), at: items.filter("completed = false").count)
-            }
-        })
-        present(alertController, animated: true, completion: nil)
+    
+    
+    deinit {
+        notificationToken?.invalidate()
     }
+    
+    
+    
 
-    
-    
-    @IBAction  func handleLogout(sender:AnyObject?) {
-        let alert = UIAlertController(title: NSLocalizedString("Logout", comment: "Logout"), message: NSLocalizedString("Really Log Out?", comment: "Really Log Out?"), preferredStyle: .alert)
-        
-        // Logout button
-        let OKAction = UIAlertAction(title: NSLocalizedString("Logout", comment: "logout"), style: .default) { (action:UIAlertAction!) in
-            SyncUser.current?.logOut()
-            //Now we need to segue to the login view controller
-            self.performSegue(withIdentifier: Constants.kExitToLoginViewSegue, sender: self)
-        }
-        alert.addAction(OKAction)
-        
-        // Cancel button
-        let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment:"cancel"), style: .cancel) { (action:UIAlertAction!) in
-            print("Cancel button tapped");
-        }
-        alert.addAction(cancelAction)
-        
-        // Present Dialog message
-        present(alert, animated: true, completion:nil)
-    }
-
-    
-    
     // MARK: Turn off the staus bar
     
     open override var prefersStatusBarHidden : Bool {
         return true
     }
-
     
-    // MARK: - Table view Data Source Methods
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
+    // MARK: UITableView
     
     override func tableView(_ tableView: UITableView?, numberOfRowsInSection section: Int) -> Int {
         return items.count
     }
-
-    // MARK: - UITableView Delegate Methods
     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let item = items[indexPath.row]
+        cell.textLabel?.text = item.text
+        cell.textLabel?.alpha = item.completed ? 0.5 : 1
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        try! items.realm?.write {
+            items.move(from: sourceIndexPath.row, to: destinationIndexPath.row)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            try! realm.write {
+                let item = items[indexPath.row]
+                realm.delete(item)
+            }
+        }
+    }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let item = items[indexPath.row]
         try! item.realm?.write {
@@ -153,59 +122,61 @@ class TasksTableViewController: UITableViewController,  UIGestureRecognizerDeleg
             items.move(from: indexPath.row, to: destinationIndexPath.row)
         }
     }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let item = items[indexPath.row]
-        cell.textLabel?.text = item.text
-        cell.textLabel?.alpha = item.completed ? 0.5 : 1
-        return cell
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
     
-    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
-        try! items.realm?.write {
-            items.move(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    // MARK: Functions
+    
+    @objc func add() {
+        let alertController = UIAlertController(title: "New Task", message: "Enter Task Name", preferredStyle: .alert)
+        var alertTextField: UITextField!
+        alertController.addTextField { textField in
+            alertTextField = textField
+            textField.placeholder = "Task Name"
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            try! realm.write {
-                let item = items[indexPath.row]
-                realm.delete(item)
-                print("here in writes")
+        alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
+            guard let text = alertTextField.text , !text.isEmpty else { return }
+            
+            let items = self.items
+            try! items.realm?.write {
+                items.insert(Task(value: ["text": text]), at: items.filter("completed = false").count)
             }
+        })
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // Logout Support
+    
+    
+    @IBAction  func handleLogout(sender:AnyObject?) {
+        let alert = UIAlertController(title: NSLocalizedString("Logout", comment: "Logout"), message: NSLocalizedString("Really Log Out?", comment: "Really Log Out?"), preferredStyle: .alert)
+        
+        // Logout button
+        let OKAction = UIAlertAction(title: NSLocalizedString("Logout", comment: "logout"), style: .default) { (action:UIAlertAction!) in
+            SyncUser.current?.logOut()
+            self.navigationController?.setViewControllers([TasksLoginViewController()], animated: true)
         }
+        alert.addAction(OKAction)
+        
+        // Cancel button
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction!) in
+            print("Cancel button tapped");
+        }
+        alert.addAction(cancelAction)
+        
+        // Present Dialog message
+        present(alert, animated: true, completion:nil)
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
